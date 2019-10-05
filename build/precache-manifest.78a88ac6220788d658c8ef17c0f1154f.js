138ffc67325431f8dc2aa9094f213522fdabcb5f
Nastasja Jovicic <jovicic.nastasja@gmail.com>
2019-10-05 14:23:07 +0300

    added build

---
+++ build/precache-manifest.78a88ac6220788d658c8ef17c0f1154f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da2f818a0b4a00a22201",
+    "url": "/sampletxtgen/static/js/runtime~main.da2f818a.js"
+  },
+  {
+    "revision": "f63fbbd6918af57f7016",
+    "url": "/sampletxtgen/static/js/main.f63fbbd6.chunk.js"
+  },
+  {
+    "revision": "ab544ac769c2c16ecc04",
+    "url": "/sampletxtgen/static/js/1.ab544ac7.chunk.js"
+  },
+  {
+    "revision": "f63fbbd6918af57f7016",
+    "url": "/sampletxtgen/static/css/main.f863d4b0.chunk.css"
+  },
+  {
+    "revision": "032606059a295e459f3558f267406de4",
+    "url": "/sampletxtgen/index.html"
+  }
+];
